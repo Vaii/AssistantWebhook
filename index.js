f40b80da66f5dbae5bbaae53ca5fb57b498a148e
Vai <vaivanommeren@gmail.com>
2018-09-27 13:08:46 +0200

    added weather fuction

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
-const checkWeather = function (arg, callback,){
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+
+    const agent = new WebhookClient({ request, response });
 
 
-    const http = require('http');
-
-    let data = '';
+    const checkWeather = function (arg, callback,){
 
 
-    http.get('http://api.openweathermap.org/data/2.5/weather?q=' + arg +'&units=metric&appid=004f84a325e90cf982bfb35ddc63c3f5', (resp) => {
+        const http = require('http');
+
+        let data = '';
 
 
-        resp.on('data', (chunk) => {
-            data += chunk;
+        http.get('http://api.openweathermap.org/data/2.5/weather?q=' + arg +'&units=metric&appid=004f84a325e90cf982bfb35ddc63c3f5', (resp) => {
+
+
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+
+
+            resp.on('end', () => {
+                callback(JSON.parse(data).min.temp)
+            });
+
+        }).on("error", (err) => {
+            callback(null, "something went wrong");
         });
 
 
-        resp.on('end', () => {
-            callback(JSON.parse(data))
-        });
 
-    }).on("error", (err) => {
-        callback(null, "something went wrong");
-    });
+    };
 
 
+    function getWeather(agent){
 
-};
+        var city = agent.parameters.City;
 
-exports.helloWorld = (req , res) => {
-
-    checkWeather("weert",  function(data) {
-
-            res.send(String(data.main.temp));
+        checkWeather(city, function(data){
+            agent.add(String(data));
+        })
     }
-)};
+
+    let intentMap = new Map();
+    intentMap.set('Weather', getWeather);
+    agent.handleRequest(intentMap);
+});
+
+
+//
+// exports.helloWorld = (req , res) => {
+//
+//     checkWeather("weert",  function(data) {
+//
+//             res.send(String(data.main.temp));
+//     }
+// )};
