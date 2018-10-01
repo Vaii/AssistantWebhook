300e948cddd763318d8ab3e4ac5deca4f85635b7
Vai <vaivanommeren@gmail.com>
2018-10-01 10:35:06 +0200

    sdk test

--- index.js
+++ index.js
@@ -5,53 +5,62 @@ const bodyParser = require('body-parser');
 const http = require('http');
 const fs = require('fs');
 
+const {dialogflow, Image} = require('actions-on-google');
+
+const app = dialogflow();
+
 const server = express();
+
 server.use(bodyParser.urlencoded({
     extended:true
 }));
 
 server.use(bodyParser.json());
 
-server.post('/assistant', (req, res) =>{
-
-    let action = req.body.queryResult && req.body.queryResult.action ;
-
-    if(action === "getWeather"){
-
-        let citytoSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.City ? req.body.queryResult.parameters.City : 'Eindhoven';
-
-        let data = '';
-        http.get('http://api.openweathermap.org/data/2.5/weather?q=' + citytoSearch +'&units=metric&appid=004f84a325e90cf982bfb35ddc63c3f5', (resp) => {
+server.post('/assistant', app =>{
 
 
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
+    app.intent('food', conv =>{
+        conv.ask(new Permission({
+            context: 'To greet you personally',
+            permissions: 'NAME',
+        }))
+    })
 
-            resp.on('end', () => {
-                console.log(JSON.parse(data).main.temp);
-                console.log(citytoSearch);
-
-                return res.json({
-                    fulfillmentText: "the weather in " + citytoSearch + " is " + String(JSON.parse(data).main.temp) + " degrees",
-                    source: 'weather'
-                });
-            });
-
-        }).on("error", (err) => {
-            console.log(err);
-            res.send(null, "something went wrong")
-        });
-    }else if(action === "getLocation"){
-
-        var obj = JSON.parse(fs.readFileSync('carrousel.json'));
-        res.send(obj);
-    }else {
-        return res.json({
-            fulfillmentText: "ok thank you for your information",
-            source: "food"
-        });
-    }
+    //let action = req.body.queryResult && req.body.queryResult.action ;
+    // if(action === "getWeather"){
+    //
+    //     let citytoSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.City ? req.body.queryResult.parameters.City : 'Eindhoven';
+    //
+    //     let data = '';
+    //     http.get('http://api.openweathermap.org/data/2.5/weather?q=' + citytoSearch +'&units=metric&appid=004f84a325e90cf982bfb35ddc63c3f5', (resp) => {
+    //
+    //
+    //         resp.on('data', (chunk) => {
+    //             data += chunk;
+    //         });
+    //
+    //         resp.on('end', () => {
+    //             console.log(JSON.parse(data).main.temp);
+    //             console.log(citytoSearch);
+    //
+    //             return res.json({
+    //                 fulfillmentText: "the weather in " + citytoSearch + " is " + String(JSON.parse(data).main.temp) + " degrees",
+    //                 source: 'weather'
+    //             });
+    //         });
+    //
+    //     }).on("error", (err) => {
+    //         console.log(err);
+    //         res.send(null, "something went wrong")
+    //     });
+    // }else if(action === "getLocation"){
+    // }else {
+    //     return res.json({
+    //         fulfillmentText: "ok thank you for your information",
+    //         source: "food"
+    //     });
+    // }
 
 });
 
